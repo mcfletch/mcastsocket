6d85bf11ef7ac1ada71d143febcfb547c527cbce
Mike C. Fletcher <mcfletch@vrplumber.com>
2016-04-21 14:48:32 -0400

    pep8 the test suite too

--- tests/test_mcastsocket.py
+++ tests/test_mcastsocket.py
@@ -7,7 +7,8 @@ test_mcastsocket
 
 Tests for `mcastsocket` module.
 """
-import select, socket
+import select
+import socket
 import unittest
 from mcastsocket import mcastsocket
 
@@ -19,47 +20,48 @@ class TestMcastsocket(unittest.TestCase):
 
     def tearDown(self):
         pass
-    def send_to_group(self,group,message,family=socket.AF_INET):
-        iface_ip = '127.0.0.1' if family== socket.AF_INET else '::1'
+
+    def send_to_group(self, group, message, family=socket.AF_INET):
+        iface_ip = '127.0.0.1' if family == socket.AF_INET else '::1'
         sock = mcastsocket.create_socket(
-            (iface_ip,8000),
+            (iface_ip, 8000),
             TTL=5,
             family=family
         )
         mcastsocket.join_group(
             sock,
-            group = group,
-            iface = iface_ip,
+            group=group,
+            iface=iface_ip,
         )
-        sock.sendto( message, (group,8000))
-        _,writable,_ = select.select([],[sock],[],.5)
+        sock.sendto(message, (group, 8000))
+        _, writable, _ = select.select([], [sock], [], .5)
         sock.close()
 
     def test_create_socket_v2(self):
         sock = mcastsocket.create_socket(
-            ('',8000),
-            TTL=5,  
+            ('', 8000),
+            TTL=5,
         )
         group = '224.1.1.2'
         mcastsocket.join_group(
             sock,
-            group = group,
+            group=group,
         )
-        self.send_to_group( group, b'moo' )
-        readable,writable,_ = select.select([sock],[],[],.5)
+        self.send_to_group(group, b'moo')
+        readable, writable, _ = select.select([sock], [], [], .5)
         assert readable, 'Nothing received'
-        (content,address) = sock.recvfrom(65000)
-        assert content == b'moo', content 
-        assert address == ('127.0.0.1',8000), address
+        (content, address) = sock.recvfrom(65000)
+        assert content == b'moo', content
+        assert address == ('127.0.0.1', 8000), address
         mcastsocket.leave_group(
             sock,
-            group = group,
+            group=group,
         )
         sock.close()
 
     def test_create_socket_v3(self):
         sock = mcastsocket.create_socket(
-            ('',8000),
+            ('', 8000),
             TTL=5,
         )
         try:
@@ -67,53 +69,55 @@ class TestMcastsocket(unittest.TestCase):
             ssm = '198.51.100.23'
             mcastsocket.join_group(
                 sock,
-                group = group,
+                group=group,
                 iface='127.0.0.1',
                 # If commented out, we get a failure,
                 # if present, we filter... yay
-                ssm = ssm,
+                ssm=ssm,
             )
             try:
-                self.send_to_group( group, b'moo' )
-                readable,writable,_ = select.select([sock],[],[],.5)
+                self.send_to_group(group, b'moo')
+                readable, writable, _ = select.select([sock], [], [], .5)
                 if readable:
-                    (content,address) = sock.recvfrom(65000)
-                assert not readable, ('Should not have received the message due to ssm filtering',address)
+                    (content, address) = sock.recvfrom(65000)
+                assert not readable, (
+                    'Should not have received the message due to ssm filtering', address)
             finally:
                 mcastsocket.leave_group(
                     sock,
-                    group = group,
+                    group=group,
                     iface='127.0.0.1',
                     # If commented out, we get a failure,
                     # if present, we filter... yay
-                    ssm = ssm,
+                    ssm=ssm,
                 )
         finally:
             sock.close()
+
     def test_ipv6_basic(self):
         sock = mcastsocket.create_socket(
-            ('',8000),
-            TTL=5,  
-            family = socket.AF_INET6,
+            ('', 8000),
+            TTL=5,
+            family=socket.AF_INET6,
         )
         assert sock.family == socket.AF_INET6, sock.family
         group = 'ff02::2'
         mcastsocket.join_group(
             sock,
-            group = group,
+            group=group,
         )
-        self.send_to_group( group, b'moo', family=socket.AF_INET6 )
-        readable,writable,_ = select.select([sock],[],[],.5)
+        self.send_to_group(group, b'moo', family=socket.AF_INET6)
+        readable, writable, _ = select.select([sock], [], [], .5)
         assert readable, 'Nothing received'
-        (content,address) = sock.recvfrom(65000)
-        assert content == b'moo', content 
-        assert address == ('::1',8000,0,0), address
+        (content, address) = sock.recvfrom(65000)
+        assert content == b'moo', content
+        assert address == ('::1', 8000, 0, 0), address
         mcastsocket.leave_group(
             sock,
-            group = group,
+            group=group,
         )
         sock.close()
-        
+
 
 if __name__ == '__main__':
     import sys
