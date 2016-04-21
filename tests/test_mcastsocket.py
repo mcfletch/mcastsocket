89ed045bd4915759dbcc77da0d9b90a517d9ba57
Mike C. Fletcher <mcfletch@vrplumber.com>
2016-04-21 13:26:05 -0400

    Make a working test-case so we can test ssm operation

--- tests/test_mcastsocket.py
+++ tests/test_mcastsocket.py
@@ -7,9 +7,8 @@ test_mcastsocket
 
 Tests for `mcastsocket` module.
 """
-
+import socket, select
 import unittest
-import netifaces
 from mcastsocket import mcastsocket
 
 
@@ -20,22 +19,39 @@ class TestMcastsocket(unittest.TestCase):
 
     def tearDown(self):
         pass
+    def send_to_group(self,group,message):
+        sock = mcastsocket.create_socket(('127.0.0.1',8000),TTL=5)
+        mcastsocket.join_group(
+            sock,
+            group = group,
+            iface = '127.0.0.1',
+        )
+        sock.sendto( message, (group,8000))
+        _,writable,_ = select.select([],[sock],[],.5)
+        sock.close()
 
     def test_create_socket_v2(self):
         sock = mcastsocket.create_socket(
-            ('127.0.0.1',8000),
+            ('',8000),
+            TTL=5,
         )
+        group = '224.1.1.2'
         mcastsocket.join_group(
             sock,
-            group = '224.1.1.2',
-            iface = '127.0.0.1',
+            group = group,
         )
+        self.send_to_group( group, b'moo' )
+        
+        readable,writable,_ = select.select([sock],[],[],.5)
+        assert readable, 'Nothing received'
+        (content,address) = sock.recvfrom(65000)
+        assert content == b'moo', content 
+        assert address == ('127.0.0.1',8000), address
         mcastsocket.leave_group(
             sock,
-            group = '224.1.1.2',
-            iface = '127.0.0.1',
+            group = group,
         )
-    
+        sock.close()
 
 if __name__ == '__main__':
     import sys
